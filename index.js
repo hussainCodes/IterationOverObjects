cc8eca10bd22d2d96d06966a8c7f72a4ec9a2a95
Hussain Alhaddad <hussain.alhaddad777@gmail.com>
2024-08-11 20:31:22 +0300

    finish objectIterations tasks

---
+++ index.js
@@ -0,0 +1,95 @@
+let person = {
+	name: "Jack",
+	age: 39,
+	city: "New York"
+}
+
+person.email = "jackomlli@git.com";
+
+function hasKey(object, key){
+	
+if ( object[key] == undefined){
+	return false;
+}else{
+	return true;
+}
+}
+// hasKey(person, "name")
+console.log(hasKey(person,"name"))
+console.log(hasKey(person,"hobby"))
+
+const movies = [
+	{
+	  title: "The Shawshank Redemption",
+	  director: "Frank Darabont",
+	  year: 1994,
+	  genre: "Drama",
+	},
+	{
+	  title: "The Godfather",
+	  director: "Francis Ford Coppola",
+	  year: 1972,
+	  genre: "Crime",
+	},
+	{
+	  title: "Pulp Fiction",
+	  director: "Quentin Tarantino",
+	  year: 1994,
+	  genre: "Crime",
+	},
+	{
+	  title: "The Dark Knight",
+	  director: "Christopher Nolan",
+	  year: 2008,
+	  genre: "Action",
+	},
+	{
+	  title: "Forrest Gump",
+	  director: "Robert Zemeckis",
+	  year: 1994,
+	  genre: "Drama",
+	},
+  ];
+  
+  function moviesList (movies){
+	movies.forEach(movie => {
+		console.log(movie.title)
+	});
+  }
+
+  moviesList(movies);
+
+  
+
+  function moviesIn1994(movies){
+	let count = 0;
+	movies.forEach((movie)=>{
+		if (movie["year"] == 1994){
+			count += 1;
+		}
+	});
+	return count;
+  }
+
+
+  console.log(moviesIn1994(movies))
+
+  function updateDarkKnight(movies){
+     movies.find((movie)=>movie.title == "The Dark Knight").genre = "Action/Drama";
+	 return movies;
+  }
+
+ // console.log(updateDarkKnight(movies))
+
+  function groupMoviesByGenre(movies){
+
+	let moviesByGenre = {
+		drama :  movies.filter((movie)=> movie.genre == "Drama"),
+		crime : movies.filter((movie)=> movie.genre == "Crime"),
+		action : movies.filter((movie)=> movie.genre == "Action")
+	}
+
+	return moviesByGenre;
+  }
+
+  console.log(groupMoviesByGenre(movies))
